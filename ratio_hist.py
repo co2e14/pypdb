0f3db95c37942335f46e147ed1c6051a6f4c2a71
co2e14 <christian.orr@diamond.ac.uk>
2022-08-04 11:39:55 +0100

    working hist

--- ratio_hist.py
+++ ratio_hist.py
@@ -4,9 +4,13 @@ import matplotlib.pyplot as plt
 columns = ["PDBID", "Scat", "Refs", "Ratio"]
 df = pd.read_csv("refScatRatio.csv", names=columns, header=None)
 df = df[df.PDBID != "None"]
-df = df[df.Ratio < 50000]
-df = df[df.Ratio > 1000]
+df = df[df.Ratio < 100000]
+#df = df[df.Ratio > 1]
 print(df)
-
-plot = plt.hist(df.Ratio, bins=1000)
+plt.xkcd()
+plot = plt.hist(df.Ratio, bins=100, log=True)
+plt.plot([1000, 1000], [0, 100000])
+plt.ylabel("# Structures (log)")
+plt.xlabel("Unique Reflections / Scatterer (S) Ratio")
+plt.title("S-SAD Solvability on I23 Based on Unique Reflections per Scatterer Ratio")
 plt.show()
