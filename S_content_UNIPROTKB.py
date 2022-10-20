8823c67b76d7bf7b9b3f9c9046b7c7ffd9ac9797
co2e14 <christian.orr@diamond.ac.uk>
2022-10-20 19:59:29 +0100

    uniprot api working

---
+++ S_content_UNIPROTKB.py
@@ -0,0 +1,35 @@
+import requests
+import re
+from requests.adapters import HTTPAdapter, Retry
+
+re_next_link = re.compile(r'<(.+)>; rel="next"')
+retries = Retry(total=5, backoff_factor=0.25, status_forcelist=[500, 502, 503, 504])
+session = requests.Session()
+session.mount("https://", HTTPAdapter(max_retries=retries))
+
+def get_next_link(headers):
+    if "Link" in headers:
+        match = re_next_link.match(headers["Link"])
+        if match:
+            return match.group(1)
+
+def get_batch(batch_url):
+    while batch_url:
+        response = session.get(batch_url)
+        response.raise_for_status()
+        total = response.headers["x-total-results"]
+        yield response, total
+        batch_url = get_next_link(response.headers)
+
+# uncompressed
+url = 'https://rest.uniprot.org/uniprotkb/search?format=fasta&query=%28%2A%29%20AND%20%28model_organism%3A9606%29&size=500'
+progress = 0
+with open('human.csv', 'w') as f:
+    for batch, total in get_batch(url):
+        lines = batch.text.splitlines()
+        if not progress:
+            print(lines[0], file=f)
+        for line in lines[1:]:
+            print(line, file=f)
+        progress += len(lines[1:])
+        print(f'{progress} / {total}')
