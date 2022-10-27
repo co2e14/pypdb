ff33be0bfabddcabc36bc02e433fb33ce2022bda
co2e14 <christian.orr@diamond.ac.uk>
2022-10-27 17:02:22 +0100

    update

--- histogram.py
+++ histogram.py
@@ -1,14 +1,17 @@
-from tkinter.tix import COLUMN
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
-df = pd.read_csv("refScatRatio.csv", names=["PDBID", "S", "RESIDUES", "RATIO"], header=None)
-df = df.dropna()
-print(df.describe())
+sns.set_style("whitegrid")
+df = pd.read_csv("refScatRatioDROPNA.csv")
+
 print(df["RATIO"].max())
-#plot = plt.hist(df2, bins=100)
-#plt.show()
-
-sns.distplot(df["RATIO"], hist=True, kde=True, rug=False)
+df.drop("Unnamed: 0", axis=1, inplace=True)
+a = np.array(df['RATIO'].values.tolist())
+df["RATIO"] = np.where(a < 2.0, 0.0, a).tolist()
+sns.kdeplot(df["RATIO"], shade=True, bw=0.05, color="#13C113", log_scale=True)
+plt.xlabel("Reflections per Sulphur Atom", fontsize=15)
+plt.ylabel("Density of Total X-Ray PDB Structures", fontsize=15)
+plt.savefig("refscatrat.png", format="png", bbox_inches="tight", dpi=600, alpha=0.5)
 plt.show()
