c1ba444ee3c2c273c9eb06a61d504a0c88ff70ee
ChrisOrr <45949926+co2e14@users.noreply.github.com>
2022-11-07 20:51:07 +0000

    pdb wavelength list working with muliple!

--- pdb_wavelength_list.py
+++ pdb_wavelength_list.py
@@ -16,7 +16,7 @@ class wavelength:
         self.returnType = ReturnType.ENTRY
         self.results = perform_search(self.searchOperator, self.returnType) 
         print(f"Found {len(self.results)} structures")
-        self.results = self.results[7000:9000]
+        self.results = self.results[8500:9000]
         return self.results
 
     def getWavelength(self, structure):
@@ -24,28 +24,40 @@ class wavelength:
         try:
             wavelength = str(info["diffrn_source"])
             for char in "[]": wavelength = wavelength.replace(char, "")
+            length = wavelength.count("pdbx_wavelength_list")
+            if length <= 1:
+                multi = False
+            elif length >= 2:
+                multi = True
             wavelength = ast.literal_eval(wavelength)
-            multi_wavelength_out = []
-            if len(wavelength) > 1:
+            if multi:
+                multi_wavelength_out = []
                 for dif_id in range(0, len(wavelength), 1):
                     multi_wavelength = wavelength[dif_id]
                     multi_wavelength_ = float(multi_wavelength["pdbx_wavelength_list"])
                     multi_wavelength_out += [multi_wavelength_]
+                wavelength = multi_wavelength_out
+            else:
+                pass
+            if length == 1:
+                wavelength = [float(wavelength["pdbx_wavelength_list"])]
+            elif length == 0:
+                wavelength = None
             else:
                 pass
         except:
-            wavelength = None
-        if wavelength == None or multi_wavelength_out == []:
+            wavelength = None  
+        if wavelength == None:
             pass
         else:
-            print(structure, multi_wavelength_out) 
-            return structure, multi_wavelength_out
+            return structure, wavelength
 
 if __name__ == '__main__':
     pool = Pool(os.cpu_count())
     getWavelengths = wavelength()
     toRun = getWavelengths.getPDBs()
     wavelengthList = list(tqdm.tqdm(pool.imap(getWavelengths.getWavelength, toRun), total=len(toRun)))
+    print(wavelengthList)
     with open("wavelengthlistvalues_all.csv", "w") as file:
         for value in wavelengthList:
             if value != None:
@@ -55,3 +67,6 @@ if __name__ == '__main__':
                     print(pdb_wave_pair)
             else:
                 pass
+            # getWavelengths = wavelength()
+    # for x in ["6tkd", "1IXG", "6qng", "6fax", "1IYQ"]:
+    #     getWavelengths.getWavelength(x)
