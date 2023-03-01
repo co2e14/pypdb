f3841198f29fde9cbbef3960a1371376e6ba5f5a
co2e14 <christian.orr@diamond.ac.uk>
2023-03-01 11:43:10 +0000

    added PDBs from paper to plot

--- pdb_wavelength_hist.py
+++ pdb_wavelength_hist.py
@@ -7,8 +7,9 @@ ax = f.add_subplot(1,1,1)
 
 columns = ["pdbid", "wavelength"]
 i23data = pd.read_csv("wavelengthlistvalues_I23.csv", names=columns, header=None)
+paperdata = pd.read_csv("wavelengthlistvalues_frompaper.csv", names=columns, header=None)
 alldata = pd.read_csv("wavelengthlistvalues_allfilt_plusneut.csv", names=columns, header=None)
 
 
-plot = plt.hist([i23data.wavelength, alldata.wavelength], color = ["orange", "grey"], stacked=True, bins=200, log=False)
+plot = plt.hist([i23data.wavelength, paperdata.wavelength, alldata.wavelength], color = ["orange", "lime", "grey"], stacked=True, bins=200, log=True)
 plt.show()
